6e5ad5b26c0d01110bac94badc8d03644e8a73f5
Waseem <mdvaseem014@gmail.com>
2023-08-17 21:06:41 +0530

    Pagination implemented on newFeed posts

---
+++ src/components/Post.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Button, Card, CardBody, CardText } from 'reactstrap'
+
+function Post({post={ title:"This is default post title", content:"This is Post Content" }}) {
+  return (
+    <Card className='border-0 shadow-sm mt-3'>
+        <CardBody>
+                <h2>{post.title}</h2>
+            <CardText>
+                { post.content.substring(0,10)}...
+            </CardText>
+
+            <div>
+                <Button>Read More</Button>
+            </div>
+        </CardBody>
+    </Card>
+  )
+}
+
+export default Post
